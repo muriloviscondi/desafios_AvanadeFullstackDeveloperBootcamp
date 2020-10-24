1639d4371855238a9559f46ee7c31a12387350ac
muriloviscondi <muriloviscondi@gmail.com>
2020-10-23 23:54:01 -0300

    Desafio Ultrapassando V

--- 3-UltrapassandoV/Program.cs
+++ 3-UltrapassandoV/Program.cs
@@ -6,7 +6,31 @@ namespace _3_UltrapassandoV
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int z = x - 1;
+
+            int i = 2;
+            int sum = x;
+            int s = 1;
+
+            while (z <= x)
+            {
+                z = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            while (sum <= z)
+            {
+                sum = sum + x + s;
+            
+                if (sum <= z)
+                {
+                    i = i + 1;
+                    s = s + 1;
+                }
+            }
+
+            Console.WriteLine(i);
         }
     }
 }
